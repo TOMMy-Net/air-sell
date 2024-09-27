d1323a3c99712cace558fba2d8906af239969b55
Timofey1234567890 <74640949+Timofey1234567890@users.noreply.github.com>
2024-09-27 19:27:35 +0300

    start pkg

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"github.com/TOMMy-Net/air-sell/internal"
+)
+
+func main() {
+	myApp := app.New()
+	//myApp.Settings().SetTheme(theme.DarkTheme())
+
+	mainWindow := myApp.NewWindow("Air Buy")
+	mainWindow.SetIcon(internal.GetIcon())
+	mainWindow.Resize(fyne.Size{Width: 1000, Height: 700})
+	mainWindow.SetMaster()
+
+	mainWindow.Show()
+	myApp.Run()
+
+}
